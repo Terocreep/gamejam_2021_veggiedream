d7187553d954196758a488a4cf8f31e78acb8a01
guillequ <quentin.guillet1@etu.univ-grenoble-alpes.fr>
2021-02-08 08:43:42 +0100

    other

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ screen = pygame.display.set_mode((720, 480))
 clock = pygame.time.Clock()
 FPS = 60  
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
 rect = pygame.Rect((0, 0), (32, 32))
 image = pygame.Surface((32, 32))
 image .fill(WHITE)  
