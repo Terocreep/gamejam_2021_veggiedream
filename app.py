7441974eee0a85b22281e32e942846d9b4dacb25
guillequ <quentin.guillet1@etu.univ-grenoble-alpes.fr>
2021-02-08 08:35:37 +0100

    other

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
 import pygame
+successes, failures = pygame.init()
+print("{0} successes and {1} failures".format(successes, failures))
+
+
+screen = pygame.display.set_mode((720, 480))
+clock = pygame.time.Clock()
+FPS = 60  
+
+rect = pygame.Rect((0, 0), (32, 32))
+image = pygame.Surface((32, 32))
+image .fill(WHITE)  
+
+while True:
+    clock.tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_z:
+                rect.move_ip(0, -2)
+            elif event.key == pygame.K_s:
+                rect.move_ip(0, 2)
+            elif event.key == pygame.K_q:
+                rect.move_ip(-2, 0)
+            elif event.key == pygame.K_d:
+                rect.move_ip(2, 0)
+
+    screen.fill(BLACK)
+    screen.blit(image, rect)
+    pygame.display.update()  # Or pygame.display.flip()
